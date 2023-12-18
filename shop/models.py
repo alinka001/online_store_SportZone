0a57755e320d709960900f1f46af288fbbfb1eec
alina <branch5567@mail.ru>
2023-12-18 13:56:02 +0300

    fix models Category, product

--- shop/models.py
+++ shop/models.py
@@ -2,17 +2,25 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-
 class Category(models.Model):
-    image = models.ImageField(upload_to='categories/', verbose_name='Изображение', blank=True)
-    description = models.TextField(blank=True, verbose_name='Описание')
+    title = models.CharField(max_length=200, verbose_name='Название',)
 
     class Meta:
         verbose_name = _("Категория")
         verbose_name_plural = _("Категории")
 
 
+class SubCategory(models.Model):
+    image = models.ImageField(upload_to='categories/', verbose_name='Изображение', blank=True)
+    title = models.CharField(max_length=200, verbose_name='Название', )
+
+    class Meta:
+        verbose_name = _("Подкатегория")
+        verbose_name_plural = _("Подкатегории")
+
+
 class Product(models.Model):
+    sub_category = models.ForeignKey(SubCategory, related_name='products', on_delete=models.CASCADE)
     title = models.CharField(max_length=200, verbose_name='Название',)
     description = models.TextField(verbose_name='Описание',)
     slug = models.CharField(unique=True, max_length=50)
@@ -21,7 +29,6 @@ class Product(models.Model):
                                     blank=True, null=True)
     image = models.ImageField(verbose_name='Изображение', upload_to='items/', blank=True)
     is_available = models.BooleanField(default=True, verbose_name='Доступно')
-
 
     def __str__(self):
         return self.title
