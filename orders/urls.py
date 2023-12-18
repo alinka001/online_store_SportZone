39bd39ef8a66bf55bc57e513fa0edd877c00f8f7
alina <branch5567@mail.ru>
2023-12-19 00:39:18 +0300

    add app 'orders'

---
+++ orders/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import order, create_order, thank_you
+
+app_name = 'order'
+
+urlpatterns = [
+    path('', order, name='order'),
+    path('create-order/', create_order, name='create_order'),
+    path('thank-you/<int:order_id>/', thank_you, name='thank_you'),
+]
