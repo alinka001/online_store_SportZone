bcba264f9e694124941f8673b7ec6c4a5917bc7d
alina <branch5567@mail.ru>
2023-12-19 01:43:16 +0300

    add app migrations

--- online_store_SportZone/settings.py
+++ online_store_SportZone/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop',
     'users',
+    'orders',
+    'cart',
+
 ]
 
 MIDDLEWARE = [
@@ -126,6 +129,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'shop:home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
